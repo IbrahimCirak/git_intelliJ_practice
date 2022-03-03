273e815f465ba09f1ac0b768df4d9953e9363c1d
Ibrahim Cirak <icirak2006@gmail.com>
2022-03-03 12:41:30 -0600

    String reverse method

--- src/test/java/java_practice/StringReverse.java
+++ src/test/java/java_practice/StringReverse.java
@@ -6,17 +6,30 @@ public class StringReverse {
 
     public static void main(String[] args){
 
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter a string");
-        String str = scan.nextLine();
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("Enter a string");
+//        String str = scan.nextLine();
+//
+//        String reverse= "";
+//        for (int i= str.length()-1; i>=0;i--){
+//            reverse=reverse + str.charAt(i);
+//        }
+//        System.out.println(reverse);
 
-        String reverse= "";
-        for (int i= str.length()-1; i>=0;i--){
-            reverse=reverse + str.charAt(i);
-        }
-        System.out.println(reverse);
+        System.out.println(reverse("hello 123"));
 
 
 
     }
+
+    public static String reverse(String str){
+        String reverse="";
+
+        for(int i= str.length()-1;i>=0;i--){
+            reverse= reverse + str.charAt(i);
+        }
+
+        return reverse;
+    }
 }
+
