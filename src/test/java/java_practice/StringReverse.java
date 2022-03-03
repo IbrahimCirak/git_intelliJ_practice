b35f4f7648594c3377c7535643b789879c0aa0ec
Ibrahim Cirak <icirak2006@gmail.com>
2022-03-03 12:21:09 -0600

    Initial commit

---
+++ src/test/java/java_practice/StringReverse.java
@@ -0,0 +1,21 @@
+package java_practice;
+
+import java.util.Scanner;
+
+public class StringReverse {
+
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a string");
+        String str = scan.nextLine();
+
+        String reverse= "";
+        for (int i= str.length()-1; i>=0;i--){
+            reverse=reverse + str.charAt(i);
+        }
+        System.out.println(reverse);
+
+
+    }
+}
