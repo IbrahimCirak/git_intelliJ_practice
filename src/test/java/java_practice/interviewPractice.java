ad87a088a36c2d085a0ef21aa19dfc3aa98cf51b
Ibrahim Cirak <icirak2006@gmail.com>
2022-03-04 15:49:35 -0600

    update

---
+++ src/test/java/java_practice/interviewPractice.java
@@ -0,0 +1,13 @@
+package java_practice;
+
+public class interviewPractice {
+
+    public static void main(String[] args) {
+
+        StringReverse stringReverse= new StringReverse();
+
+        System.out.println(stringReverse.reverse("hello how are you?"));
+
+        System.out.println(stringReverse.wordReverse("hello how are you?"));
+    }
+}
